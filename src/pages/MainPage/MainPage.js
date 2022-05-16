dbf9dcdcea4c2e21080740ac622139242ee9f126
habibmustafa <hebibullahmustafazade@gmail.com>
2022-05-16 18:07:08 +0400

    add: responsive styles

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.css';
 import Header from '../../components/Header/Header';
 import SearchBox from '../../components/SearchBox/SearchBox';
@@ -6,9 +6,13 @@ import Movies from '../../components/Movies/Movies';
 import Favorites from '../../components/Favorites/Favorites';
 
 const MainPage = () => {
+   const [show, setShow] = useState(false)
    return (
       <div className="main-page">
          <Header />
+         <button className="favorite-show-btn" onClick={() => { setShow(!show) }}>
+            {!show ? "Sevimli filmləri göstər" : "Sevimli filmləri gizlət"}
+         </button>
          <main className="main-page__content">
             <section className="main-page__main-section">
                <div className="main-page__search-box">
@@ -18,7 +22,7 @@ const MainPage = () => {
                   <Movies />
                </div>
             </section>
-            <aside className="main-page__favorites">
+            <aside className={"main-page__favorites" + (!show ? " main-page__favorites-hide" : '')}>
                <Favorites />
             </aside>
          </main>
